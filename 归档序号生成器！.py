bb71faab23eae9348cc333a9b875dbb60d2c4eec
shuiping233 <1944680304@qq.com>
2022-02-27 07:47:43 +0800

    update 归档序列号生成器.py

--- 归档序号生成器！.py
+++ 归档序号生成器！.py
@@ -32,22 +32,20 @@ for Times in range(1000000000):
 
 if issuesType<=3 or issuesType>=1 and issueOurin>=1 or issueOurin<=2:
     # 判断前边两个输入条件是否满足，虽然前边已经优化过，现在没必要在这里判断了，但是我懒得的改动了
-    SerialNum = int(input("输入序号："))
-    VerisonNun = str(input("输入版本号0.99.90"))
-    x = 2
-    # 不知道干啥的，可以删掉
-    IssuesNum = int(input("请输入issue号：#"))
+    SerialNum = int(input("输入issue归档起始序号："))
+    VerisonNun = str(input("输入版本号0.99."))
+    IssuesNum = int(input("请输入issue归档单号："))
     for Times in range(1000000000):
        RangeTimes = int(input("输入循环次数（数值不能超过1000）:"))
        # 防止手贱输入超过1000以上的数值导致cpu着火
        if RangeTimes<=1000:
         for i in range(SerialNum, SerialNum + RangeTimes + 1):
-           print("|" + str(i) + "" + "|" + "" + "(" + str(IssuesType) + ")" + str(Archiveformat) + "[" + str(IssueOurin) + "部Issue#" +str(IssuesNum)+ "] " + "                                            " + "||0.99.90" + str(VerisonNun) + "|")
+           print("|" + str(i) + "" + "|" + "" + "(" + str(IssuesType) + ")" + str(Archiveformat) + "                                       " + "[" + str(IssueOurin) + "部Issue#" +str(IssuesNum)+ "] " + "||0.99.90" + str(VerisonNun) + "|")
            data = open("归档预备.md", 'w+')
            # 打印结果
            for i in range(SerialNum, SerialNum + RangeTimes + 1):
                # 我没想到i要怎么用英语表述，是用来输出的中间量
-              print("|" + str(i) + "" + "|" + "" + "(" + str(IssuesType) + ")" + str(Archiveformat) + "[" + str(IssueOurin) + "部Issue#" +str(IssuesNum)+ "] " + "                                            " + "||0.99.90" + str(VerisonNun) + "|", file=data)
+              print("|" + str(i) + "" + "|" + "" + "(" + str(IssuesType) + ")" + str(Archiveformat) + "                                       " + "[" + str(IssueOurin) + "部Issue#" +str(IssuesNum)+ "] " + "||0.99.90" + str(VerisonNun) + "|", file=data)
            data.close()
         print("输出完成，请检查此py文件根目录下的'归档预备.md'文件")
         # 打印结果到此py文件下目录的“归档预备.md”
